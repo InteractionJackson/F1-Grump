6638d96785bbaa1af08e44d11c50ee4f1feccaba
Matt Jackson <matt@interactionjackson.com>
2025-09-14 01:38:19 +0100

    Bug fix

--- F1 Grump/ContentView.swift
+++ F1 Grump/ContentView.swift
@@ -143,18 +143,16 @@ struct ContentView: View {
         GeometryReader { colGeo in
             let spacing: CGFloat = 16
             let hAvailable = colGeo.size.height - spacing
-            let condH = max(0, condTileHeight)
+            let groupSpacing: CGFloat = 24   // spacing between FL/RL and FR/RR groups
+            let tyreRowH: CGFloat = 32       // fixed height per TyreRow
+            let stackInnerSpacing: CGFloat = 8
+            let tyreStackH = tyreRowH * 3 + stackInnerSpacing * 2 // three rows + 2 inner spacings
+            let condContentH = max(200, tyreStackH * 2 + groupSpacing) // hug content (car vs two stacks)
+            let condH = min(condContentH, hAvailable) // don't exceed available
             let speedH = max(0, hAvailable - condH)
             VStack(alignment: .leading, spacing: spacing) {
                 Card(title: "Car condition & damage", height: condH) {
                     CarConditionGrid(temps: rx.tyreInnerTemps, wear: rx.tyreWear, brakes: rx.brakeTemps)
-                        .background(
-                            GeometryReader { proxy in
-                                Color.clear
-                                    .onAppear { condTileHeight = proxy.size.height }
-                                    .onChange(of: proxy.size.height) { _, new in condTileHeight = new }
-                            }
-                        )
                 }
 
                 Card(title: "Speed, RPM, DRS & Gear", height: speedH) {
